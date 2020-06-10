ac2876636df15d62bb46075bfb49d8a51abc4610
shindje <aleks.shindje@gmail.com>
2020-06-10 21:38:09 +0300

    Homework 8 init

---
+++ src/hw8/server/AuthService.java
@@ -0,0 +1,6 @@
+package hw8.server;
+
+public interface AuthService {
+    String getNicknameByLoginAndPassword(String login, String password);
+    boolean registration(String login, String password, String nickname);
+}
